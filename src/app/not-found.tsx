b30eb8e3d5f09dda334ff8da629f5446968c1a53
ardhanurfan <ardhanurfan@gmail.com>
2025-01-03 22:59:12 +0700

    feat: implement not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="text-center p-4">
+      <h2 className="text-2xl font-semibold mb-4">Not Found</h2>
+      <p className="text-lg mb-4">Could not find the requested resource.</p>
+      <Link
+        href="/"
+        className="bg-black px-4 py-1 rounded-lg text-white text-sm"
+      >
+        Return Home
+      </Link>
+    </div>
+  );
+}
