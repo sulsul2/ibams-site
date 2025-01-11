92bb469c600be27ced03a0333075f0f24d92afef
ardhanurfan <ardhanurfan@gmail.com>
2025-01-11 12:14:33 +0700

    off eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
